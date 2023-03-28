782cd8100a87e7b43563ddcfc23950438035c21c
EnzoVe <enzovenon@laposte.net>
2023-03-28 15:58:39 +0200

    Add style button event listener. Not functional yet

--- src/main.js
+++ src/main.js
@@ -117,3 +117,84 @@ for (const layer of view.getLayers()) {
 
 }
 
+
+let meshes = [];
+// Je crois que je suis obligé de faire l'event listener ici pour avoir accès à la constante view
+// Fonction qui applique un style à la couche BD Topo
+document.getElementById("bouton_ok_style").addEventListener('click', () => {
+    meshes = [];
+    // On récupère les infos de style
+    const champ = document.getElementById("in_ch1Topo").value;
+    const valeur = document.getElementById("in_v1Topo").value;
+    const couleur = document.getElementById("in_co1Topo").value;
+
+    // Nouvelle fonction colorBuildings qui correspond aux infos
+    function colorBuildings(properties) {
+        let color = new itowns.THREE.Color();
+        if (properties[champ] === valeur) {
+            return color.set(couleur);
+        }
+
+        return color.set(0x555555);
+    }
+
+    // Pas changée Placing the data on the ground
+    function altitudeBuildings(properties) {
+        return properties.altitude_minimale_sol;
+    }
+
+    // Pas changée Extruding the data 
+    function extrudeBuildings(properties) {
+        return properties.hauteur;
+    }
+
+    // Pas changée 
+    function acceptFeature(properties) {
+        return !!properties.hauteur;
+    }
+
+    // Comme iTowns ne possède pas de modification dynamique de style, on supprime la couche et on la recrée
+    const layer = view.getLayerById('WFS Building');
+    view.removeLayer('WFS Building');
+    layer.delete();
+
+    // Source
+    const geometrySource = new itowns.WFSSource({
+        url: 'https://wxs.ign.fr/topographie/geoportail/wfs?',
+        version: '2.0.0',
+        typeName: 'BDTOPO_V3:batiment',
+        crs: 'EPSG:4326',
+        ipr: 'IGN',
+        format: 'application/json',
+        extent: {
+            west: 0.67289,
+            east: 0.74665,
+            south: 45.17272,
+            north: 45.2135,
+        }
+    });
+
+    // Geometry Layer
+    const geomLayer = new itowns.FeatureGeometryLayer('WFS Building', {
+        batchId: function (property, featureId) { return featureId; },
+        onMeshCreated: function scaleZ(mesh) {
+            mesh.children.forEach(c => {
+                c.scale.z = 0.01;
+                meshes.push(c);
+            })
+        },
+        filter: acceptFeature,
+        source: geometrySource,
+        zoom: { min: 14 },
+
+        style: new itowns.Style({
+            fill: {
+                color: colorBuildings,
+                base_altitude: altitudeBuildings,
+                extrusion_height: extrudeBuildings,
+            }
+        })
+    });
+
+    view.addLayer(geomLayer);
+});
