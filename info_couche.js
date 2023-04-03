6bdb55b21fc850f386d63c6703bacfe0ed580648
Kingatsar <87530643+Kingatsar@users.noreply.github.com>
2023-04-03 10:26:53 +0200

    adding layers info

---
+++ info_couche.js
@@ -0,0 +1,82 @@
+// // couche com
+    // CAPITALE: "Non"
+    // CL_ARROND: "Oui"
+    // CL_COLLTER: "Oui"
+    // CL_DEPART: "Oui"
+    // CL_REGION: "Non"
+    // CODE_POST: "24000"
+    // CODE_SIREN: "212403224"
+    // DATE_APP: Date Thu Nov 30 1899 00: 00: 00 GMT + 0009(heure normale d’Europe centrale)
+    // DATE_CONF: Date Thu Nov 30 1899 00: 00: 00 GMT + 0009(heure normale d’Europe centrale)
+    // DATE_CREAT: "2006-08-02 08:36:54"
+    // DATE_MAJ: "2022-05-16 17:06:46"
+    // DATE_RCT: Date Tue Jan 01 2019 00: 00: 00 GMT + 0100(heure normale d’Europe centrale)
+    // ID: "COMMUNE_0000000009754332"
+    // ID_AUT_ADM: "SURFACTI0000000049445044"
+    // ID_CH_LIEU: "PAIHABIT0000000049513040"
+    // INSEE_ARR: "3"
+    // INSEE_COL: "24D"
+    // INSEE_COM: "24322"
+    // INSEE_DEP: "24"
+    // INSEE_REG: "75"
+    // NOM: "Périgueux"
+    // POPULATION: 29896
+    // RECENSEUR: "INSEE"
+    // SIREN_EPCI: "200040392"
+    // SURFACE_HA: 982
+
+// // Couche bdnb
+    // altitude_s: 85
+    // batiment_c: "BATIMENT0000000295374065-5"
+    // batiment_g: "24322000BR0860_bb78e9ae8f26407"
+    // code_commu: "24322"
+    // code_depar: "24"
+    // code_iris: "243220105"
+    // fictive_ge: 0
+    // fid: 24484
+    // hauteur: 6.2
+    // s_geom_cst: 616
+
+// // couche osm
+    // code: 1500
+    // fclass: "building"
+    // name: ""
+    // osm_id: "75342558"
+
+
+    // // couche bd topo
+    // ACQU_ALTI: "Interpolation bâti BDTopo"
+    // ACQU_PLANI: "BDParcellaire recalée"
+    // APP_FF: "A 1.0"
+    // DATE_APP: Date Mon Jan 01 1900 00: 00: 00 GMT + 0009(heure normale d’Europe centrale)
+    // DATE_CONF: Date Thu Nov 30 1899 00: 00: 00 GMT + 0009(heure normale d’Europe centrale)
+    // DATE_CREAT: "2012-02-13 11:09:07"
+    // DATE_MAJ: "2022-05-13 00:43:33"
+    // ETAT: "En service"
+    // HAUTEUR: 6.2
+    // ID: "BATIMENT0000000295374065"
+    // ID_SOURCE: ""
+    // LEGER: "Non"
+    // MAT_MURS: ""
+    // MAT_TOITS: ""
+    // NATURE: "Indifférenciée"
+    // NB_ETAGES: 1
+    // NB_LOGTS: 0
+    // ORIGIN_BAT: "Cadastre"
+    // PREC_ALTI: 2.5
+    // PREC_PLANI: 3
+    // SOURCE: ""
+    // USAGE1: "Commercial et services"
+    // USAGE2: ""
+    // Z_MAX_SOL: 86.5
+    // Z_MAX_TOIT: 93
+    // Z_MIN_SOL: 85
+    // Z_MIN_TOIT: 91.2
+
+// // couche cadastre
+    // commune: "24322"
+    // created: Date Mon Oct 23 2006 00: 00: 00 GMT + 0200(heure d’été d’Europe centrale)
+    // nom: ""
+    // style: Object { isStyle: true, order: 0, parent: {… }, … }
+    // type: "01"
+    // updated: Date Tue Jun 12 2018 00: 00: 00 GMT + 0200(heure d’été d’Europe centrale)
