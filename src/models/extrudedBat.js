5ea6f02d31da843039a5eb80f5aaf049119ea341
GITGT200 <tiazaragaetan@gmail.com>
2023-04-03 21:13:00 +0200

    data from bdnb in the panel

--- src/models/extrudedBat.js
+++ src/models/extrudedBat.js
@@ -7,62 +7,63 @@ Color for the object
 */
 export function addSpecificBuilings(databaseType, height, properties, valuesOfPerperties, color, view) {
 
+    shapefile.open(databaseType)
+        .then(source => source.read()
+            .then(function log(result) {
+                if (result.done) return;
+                try {
+                    // console.log(result.value.properties[properties])
+                    // console.log(valuesOfPerperties)
 
 
-    if (databaseType === "osm") {
-        shapefile.open("../data/shp/prg/osm.shp")
-            .then(source => source.read()
-                .then(function log(result) {
-                    if (result.done) return;
-                    // console.log(result.value)
-                    try {
-                        if (result.value.properties[properties] === valuesOfPerperties) {
-                            let src2 = new itowns.FileSource({
-                                fetchedData: {
-                                    "type": "FeatureCollection",
-                                    "features": [
-                                        {
-                                            "type": "Feature", "properties": { "gid": 80, "station": "MEAUX", "hdysf": 4.860000 }, "geometry": {
-                                                "type": "Polygon", "coordinates": [result.value.geometry.coordinates[0]]
-                                            }
-                                        }]
-                                },
-
-                                crs: 'EPSG:4326',
-                                format: 'application/json',
-                            })
-
-                            if (!view.getLayerById(result.value.properties.osm_id)) {
-                                let bat = new itowns.FeatureGeometryLayer(result.value.properties.osm_id, {
-                                    source: src2,
-                                    transparent: true,
-                                    opacity: 0.7,
-                                    zoom: { min: 0 },
-                                    style: new itowns.Style({
-                                        fill: {
-                                            color: color,
-                                            extrusion_height: height,
+                    if (result.value.properties[properties] === valuesOfPerperties) {
+                        let src2 = new itowns.FileSource({
+                            fetchedData: {
+                                "type": "FeatureCollection",
+                                "features": [
+                                    {
+                                        "type": "Feature", "properties": { "gid": 80, "station": "MEAUX", "hdysf": 4.860000 }, "geometry": {
+                                            "type": "Polygon", "coordinates": [result.value.geometry.coordinates[0]]
                                         }
-                                    })
-                                });
+                                    }]
+                            },
 
-                                view.addLayer(bat)
+                            crs: 'EPSG:4326',
+                            format: 'application/json',
+                        })
+                        console.log(result.value.geometry.coordinates[0])
 
-                                document.getElementById('bat').value = { coord: result.value.geometry.coordinates[0] }
-                            }
+                        if (!view.getLayerById(result.value.properties[properties])) {
+                            let bat = new itowns.FeatureGeometryLayer(result.value.properties[properties], {
+                                source: src2,
+                                transparent: true,
+                                opacity: 0.7,
+                                zoom: { min: 0 },
+                                style: new itowns.Style({
+                                    fill: {
+                                        color: color,
+                                        extrusion_height: height,
+                                    }
+                                })
+                            });
 
+                            view.addLayer(bat)
 
-
-
-
+                            document.getElementById('bat').value = { coord: result.value.geometry.coordinates[0] }
                         }
-                    } catch (e) {
-                        console.log(e)
+
+
+
+
+
                     }
-                    return source.read().then(log);
-                }))
-            .catch(error => console.error(error.stack));
-    }
+                } catch (e) {
+                    console.log(e)
+                }
+                return source.read().then(log);
+            }))
+        .catch(error => console.error(error.stack));
+
 }
 
 
