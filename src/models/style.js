3ef7290c059d4b8a499de05a718da6fa13735d26
EnzoVenon <enzovenon@laposte.net>
2023-04-05 12:15:40 +0200

    Starting to add automated min and max to setter

--- src/models/style.js
+++ src/models/style.js
@@ -28,15 +28,22 @@ export default class Style {
      * Sets the color(s) and min and max values to use for gradation.
      * @param {String} color1 Color of the gradation.
      * @param {String} color2 Second color, in case you want for example to go from blue to red. Default value is "".
-     * @param {Boolean} auto_min_max Whether to automatically detect the min and the max of the field. Default is true.
      * @param {Number} min If you want the white or the second color to be set to a specific min rather than it being detected automatically. Default is NaN (so the min is automatically detected).
      * @param {Number} max If you want your color to be set to a specific max rather than it being detected automatically. Default is NaN (so the max is automatically detected).
      */
     setGradation(color1, color2 = "", min = NaN, max = NaN) {
         this.color1 = color1;
         this.color2 = color2;
-        this.min = min;
-        this.max = max;
+        if (isNaN(min)) {
+            //Trouver et set le min automatiquement
+        } else {
+            this.min = min;
+        }
+        if (isNaN(max)) {
+            //Trouver et set le max automatiquement
+        } else {
+            this.max = max;
+        }
         return this;
     }
 
