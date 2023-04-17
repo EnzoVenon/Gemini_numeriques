33a7324ea04e730254959dedb98dc56c54853234
kingatsa <jcribaira@outlook.com>
2023-04-17 13:49:12 +0200

    optim pop data generation

--- src/main.js
+++ src/main.js
@@ -180,33 +180,27 @@ viewerDiv.addEventListener(
                             })
 
                             // Chart for INSEE values
-                            // ----- Status 15 ans et plus ----- //
                             const dataList4Chart = {
                                 status15OuPlus: ['P19_POP15P_MARIEE', 'P19_POP15P_PACSEE', 'P19_POP15P_CONCUB_UNION_LIBRE', 'P19_POP15P_VEUFS', 'P19_POP15P_DIVORCEE', 'P19_POP15P_CELIBATAIRE'],
                                 repartitionPop: ['P19_POP1524', 'P19_POP2554', 'P19_POP5579', 'P19_POP80P'],
                                 enfant25: ['C19_NE24F0', 'C19_NE24F1', 'C19_NE24F2', 'C19_NE24F3', 'C19_NE24F4P']
                             }
+                            let data4Chart = [];
+                            Object.entries(dataList4Chart).forEach(([key, value]) => {
+                                data4Chart.push(contenuOnglet.dataINSEE4Chart(value, valDisplayedPop.tabPopulation))
+                            })
 
-                            let data4Chart = {
-
-                            }
-                            const dataRelation15 = contenuOnglet.dataINSEE4Chart(dataList4Chart.status15OuPlus, valDisplayedPop.tabPopulation);
+                            // ----- Generate HTML text ----- //
                             textHtml += contenuOnglet.generateAccordionItem("Status_15_ans+", 'status');
-
-                            // ----- Répartition pop 15 ans et plus ----- //
-                            const dataRepartitionPop = contenuOnglet.dataINSEE4Chart(dataList4Chart.repartitionPop, valDisplayedPop.tabPopulation);
                             textHtml += contenuOnglet.generateAccordionItem("Repartion_pop_15_ans+", 'repartition');
-
-                            // ----- Nombre de familles avec enfants -25 ans ----- //
-                            const dataEnfant25 = contenuOnglet.dataINSEE4Chart(dataList4Chart.enfant25, valDisplayedPop.tabPopulation);
                             textHtml += contenuOnglet.generateAccordionItem("Nombre_famille_enfants_-25ans", 'enfant');
 
                             htmlTest.innerHTML += textHtml;
 
                             // Create charts
-                            addChart('status', dataRelation15, 'name', 'value', 'Nombre de personnes');
-                            addChart('repartition', dataRepartitionPop, 'name', 'value', "Nombre d'individus");
-                            addChart('enfant', dataEnfant25, 'name', 'value', 'Nombre de familles');
+                            addChart('status', data4Chart[0], 'name', 'value', 'Nombre de personnes');
+                            addChart('repartition', data4Chart[1], 'name', 'value', "Nombre d'individus");
+                            addChart('enfant', data4Chart[2], 'name', 'value', 'Nombre de familles');
 
                             return valDisplayedPop;
                         })
