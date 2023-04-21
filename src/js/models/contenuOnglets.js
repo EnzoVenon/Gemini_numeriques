abb9e18156957d029a43bc5f41e1217fb100e5d0
kingatsa <jcribaira@outlook.com>
2023-04-21 11:49:22 +0200

    cleaning code

--- src/js/models/contenuOnglets.js
+++ src/js/models/contenuOnglets.js
@@ -14,10 +14,7 @@ export function dataINSEE4Chart(attributeList, popValues) {
             result.push({ name: value.name4User, value: value.val })
         }
     })
-    // attributeList.filter(function (attribute) {
 
-    //     result.push({ name: attribute.name4User, value: attribute.val })
-    // });
     return result;
 }
 
@@ -50,3 +47,56 @@ export function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
 }
 
+function createAccParent(id) {
+    let accParent = document.createElement("div")
+    accParent.className = "accordion"
+    accParent.id = id
+
+    return accParent
+}
+
+function createAccChild(id, header, body) {
+    let accChild = document.createElement("div")
+    accChild.className = "accordion-item"
+    accChild.id = id
+
+    accChild.id = id
+    accChild.innerHTML += `<h2 class="accordion-header" id="panelsStayOpen-heading${id}">
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+        data-bs-target="#panelsStayOpen-collapse${id}" aria-expanded="false"
+        aria-controls="panelsStayOpen-collapse${id}">
+        ${header}
+    </button>
+</h2>
+<div id="panelsStayOpen-collapse${id}" class="accordion-collapse collapse"
+    aria-labelledby="panelsStayOpen-heading${id}">
+    <div class="accordion-body">
+    ${body}
+    </div>
+</div>`
+
+    return accChild
+}
+
+export function createAccordion(idParent, header, body) {
+    let divParent = createAccParent(idParent)
+    let divChild = createAccChild(idParent + 'child', header, body)
+    divParent.appendChild(divChild)
+    return divParent
+}
+
+export function createAccordionForListAttributes(listAttributes, isForIndividuals = false) {
+    let body = '';
+    listAttributes.forEach((value) => {
+        if (value) {
+            if (isForIndividuals) {
+                body += value.name4User + ": " + value.val + '<br>'
+            } else {
+
+                body += createAccordion(value.attribute, value.name4User, value.val).outerHTML
+            }
+
+        }
+    })
+    return body
+}
